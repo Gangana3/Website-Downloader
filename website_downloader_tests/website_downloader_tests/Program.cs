3433e450bdbd823bbba192274ee9a5b8dbb787b3
Gangana3 <organy110@gmail.com>
2018-12-31 15:51:03 +0200

    Changing implementation of DownloadImgs, adding implementation of DownloadJS, adding TODO notes for next commits

--- website_downloader_tests/website_downloader_tests/Program.cs
+++ website_downloader_tests/website_downloader_tests/Program.cs
@@ -12,10 +12,7 @@ namespace website_downloader_tests
     {
         static void Main(string[] args)
         {
-            var urlDownloader = new UrlDownloader("https://he.wikipedia.org/wiki/%D7%91%D7%A0%D7%99%D7%9E%D7%99%D7%9F_%D7%A0%D7%AA%D7%A0%D7%99%D7%94%D7%95");
-
-
-            urlDownloader.DownloadImages(@"C:\Users\ganga\Documents\try");
+            Console.WriteLine(UrlDownloader.GetAbsoluteUrl("https://www.google.com/foo1/foo2/foo3", "../../stylesheet.css"));
         }
     }
 }
--- website_downloader_tests/website_downloader_tests/UrlDownloader.cs
+++ website_downloader_tests/website_downloader_tests/UrlDownloader.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 using System.Net;
 using System.IO;
 
+/// <summary>
+/// TODO: Make the class work for multithreaded download. for example the css, the js and the imgs may
+/// be downloaded each from a different thread. Maybe split the three of these into groups.
+/// 
+/// TODO: Check the DownloadImages and DownloadJS functions.
+/// </summary>
 namespace website_downloader_tests
 {
     /// <summary>
@@ -15,14 +21,18 @@ namespace website_downloader_tests
     /// </summary>
     class UrlDownloader
     {
+        private static readonly object lockObj = new object();
+
         // Public Properties
         public string HtmlCode { get; private set; }    // Html code
-
-        public string Url { get; private set; }
+        public string Url { get; private set; }         // Url to download
 
         // Private fields
-        private HtmlDocument htmlDoc;
-        private WebClient webClient;
+        private HtmlDocument htmlDoc;                           // Used for analyzing the html page
+        private WebClient webClient;                            // Used for downloading the html (by the given url)
+        private int resourceId = 0;                             // Used for naming the resources
+        private Dictionary<string, string> resourcesNames;      // Used for mapping between the downloaded url and local resource path
+
 
         /// <summary>
         /// Instantiate a UrlDownloader object
@@ -57,14 +67,44 @@ namespace website_downloader_tests
             int id = 0;
             foreach (HtmlElement imgTag in htmlDoc.GetElementsByTagName("img"))
             {
-                string href = imgTag.Attributes["src"];
-                string url = GetAbsoluteUrl(this.Url, href);
+                string href = imgTag.Attributes["src"];         // Get the href attribute from the element
+                string url = GetAbsoluteUrl(this.Url, href);    // Get the absolute url from the given url
 
                 string fileName = Path.Combine(path, id.ToString());
-                id++;
-                Console.WriteLine("Downloading {0} and saves it as {1}", url, fileName);     // Debug message
+                this.RegisterDownload(url, fileName);
                 this.webClient.DownloadFile(url, fileName);
             }
+        }
+
+        /// <summary>
+        /// Downloads the Stylesheets from the given url, Including the resources that
+        /// The stylesheet uses like another stylesheets, images etc...
+        /// </summary>
+        public void DownloadStylesheets(string path)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Downloads the javascript from the html page
+        /// </summary>
+        public void DownloadJS(string path)
+        {
+            foreach (HtmlElement element in htmlDoc.GetElementsByTagName("script"))
+            {
+                // In case the script element has an 'src' attribute
+                if (element.Attributes.Keys.Contains("src"))
+                {
+                    string src = element.Attributes["src"];
+                    string url = GetAbsoluteUrl(this.Url, src);
+
+                    string fileName = Path.Combine(path, this.resourceId.ToString());
+                    this.webClient.DownloadFile(url, fileName);
+                    this.RegisterDownload(url, fileName);
+                }
+            }
+                
         }
 
 
@@ -76,7 +116,7 @@ namespace website_downloader_tests
         /// </summary>
         /// <param name="baseUrl"></param>
         /// <param name="relativeUrl"></param>
-        private static string GetAbsoluteUrl(string currentUrl, string relativeUrl)
+        public static string GetAbsoluteUrl(string currentUrl, string relativeUrl)
         {
             // Get base url, for example 'https://www.google.com/foo/foo1/foo2' -> base url = 'https://www.google.com'
             string baseUrl = Regex.Match(currentUrl, @"https?://[a-zA-Z\.0-9]*").Groups[0].Value;
@@ -98,10 +138,38 @@ namespace website_downloader_tests
                 return relativeUrl;
             // In case the relative url starts with '..'
             else if (relativeUrl.StartsWith(".."))
-                return currentUrl.Slice(0, currentUrl.LastIndexOf('/', currentUrl.Length - 2) + 1) + relativeUrl;
+            {
+                string resultUrl = currentUrl;
+                // In case url ends with '/', remove it
+                if (relativeUrl.EndsWith("/"))
+                    relativeUrl.Remove(relativeUrl.Length - 1);
+                // Move backwards in the url
+                while (relativeUrl.StartsWith(".."))
+                {
+                    resultUrl = resultUrl.Remove(resultUrl.LastIndexOf('/'));
+                    relativeUrl = relativeUrl.Substring(relativeUrl.IndexOf('/') + 1);
+                }
+                resultUrl += "/" + relativeUrl;     // Add the relative url to the result
+                return resultUrl
+            }
             // In case the relative url is just a relative url
             else
                 return currentUrl + relativeUrl;
+        }
+
+        /// <summary>
+        /// Used to mark a url as downloaded, In order to be able to retreive it's file name
+        /// and insert it to the page.
+        /// </summary>
+        /// <param name="absoluteUrl">Absolute url to the downloaded file</param>
+        /// <param name="resourceId">Id of the given file</param>
+        private void RegisterDownload(string absoluteUrl, string filePath)
+        {
+            lock (lockObj)
+            {
+                this.resourcesNames.Add(absoluteUrl, filePath);     // Add the file to the list
+                this.resourceId++;                                  // Move on to the next file
+            }
         }
         #endregion
     }
