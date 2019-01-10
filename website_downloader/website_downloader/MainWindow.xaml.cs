af138be21e6907a7b3e0b5dfd84eca62511c386f
Gangana3 <organy110@gmail.com>
2019-01-10 14:53:11 +0200

    Adding design for the main window

--- website_downloader/website_downloader/MainWindow.xaml.cs
+++ website_downloader/website_downloader/MainWindow.xaml.cs
@@ -20,9 +20,36 @@ namespace website_downloader
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Loads the 2 download images, one for when the download button is active, and the other for inactive
+        private static BitmapImage downloadActive = new BitmapImage(new Uri("Images/download1.png", UriKind.Relative));
+        private static BitmapImage downloadInactive = new BitmapImage(new Uri("Images/download2.png", UriKind.Relative));
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Executes when the mouse enters the download image
+        /// </summary>
+        /// <param name="sender">event sender</param>
+        /// <param name="e">event args</param>
+        private void DownloadImageMouseEnter(object sender, MouseEventArgs e)
+        {
+            // Show the active image of the download button
+            this.downloadImage.Source = downloadActive;
+        }
+
+        /// <summary>
+        /// Executes when the mouse leaves the download image
+        /// </summary>
+        /// <param name="sender">event sender</param>
+        /// <param name="e">event args</param>
+        private void DownloadImageMouseLeave(object sender, MouseEventArgs e)
+        {
+            // Show the inactive image of the download button
+            this.downloadImage.Source = downloadInactive;
         }
     }
 }
